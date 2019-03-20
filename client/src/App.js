401e3fd8118f7ff08fb2dfc2ec1258709564e08e
Rohit Dhiman <aimflaiims@gmail.com>
2019-03-20 18:53:39 +0530

    register login comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// import components
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My reat app</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route path="/" exact component={Landing} />
+          <div className="container">
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
