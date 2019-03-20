2efac0e8657a39bfd9f3a91700b0c6ce0ace52f6
Rohit Dhiman <aimflaiims@gmail.com>
2019-03-20 19:13:58 +0530

    heroku

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://aimflaiims:illuminator@aimflaiims-ssgze.mongodb.net/devconnector?retryWrites=true",
-  secret: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
