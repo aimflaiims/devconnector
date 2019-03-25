800f9c4323cd10a764701ed6e50cde63816d6f91
Rohit Dhiman <aimflaiims@gmail.com>
2019-03-25 15:14:09 +0530

    post reducer and actions

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case value:
+      break;
+
+    default:
+      return state;
+  }
+}
